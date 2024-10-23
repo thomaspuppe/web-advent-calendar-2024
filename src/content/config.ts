d107d2308d749dbe3cc6badc1c0b14489d487021
thomaspuppe <info@thomaspuppe.de>
2024-10-23 22:08:16 +0200

    Model the calendar as content collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,4 +13,17 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const calendar = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		category: z.string(),
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+	}),
+});
+
+export const collections = { blog, calendar };
