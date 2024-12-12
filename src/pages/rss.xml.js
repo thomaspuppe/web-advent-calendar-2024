4c96afe9f1faf05df77551acc9c2bcbaf58a1fd4
thomaspuppe <info@thomaspuppe.de>
2024-12-12 19:45:54 +0100

    Fix RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,17 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = (await getCollection('calendar')).
+		filter((post) => post.data.published ).
+		sort((a, b) => b.data.slug - a.data.slug);
+		
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.slug}/`,
+			link: `/${post.slug}/`,
 		})),
 	});
 }
